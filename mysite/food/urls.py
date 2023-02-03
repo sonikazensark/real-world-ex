7215bcd6ced16128cc615c19e2bd34ef7a1c19e5
sonikazensark <sonika.mourya@zensark.com>
2023-02-03 10:53:08 +0530

    Initial commit

---
+++ mysite/food/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+app_name ='food'
+urlpatterns = [
+    path('',views.index,name ='index'),
+    path('item/',views.item,name ='item'),
+    path('<int:item_id>/',views.detail,name ='detail'),
+    
+
+]
